ad89be582e52d4bc3c610b5267bb99a42429fdec
hackathon-develop <123299658+hackathon-develop@users.noreply.github.com>
2026-08-14 20:16:14 +0200

    port adjust

--- cognee-demo-slack/app.py
+++ cognee-demo-slack/app.py
@@ -95,4 +95,4 @@ async def slack_commands(request: Request, background_tasks: BackgroundTasks):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
